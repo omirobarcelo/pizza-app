d47be3484224767fb2956c27356ca59d8d4f6bd9
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2019-11-20 14:44:31 +0100

    Init Svelte app

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import '../node_modules/@clr/ui/clr-ui.min.css';
+import '../node_modules/@clr/icons/clr-icons.min.css';
+import '../node_modules/@webcomponents/custom-elements/custom-elements.min.js';
+import '../node_modules/@clr/icons/clr-icons.min.js';
+
+import App from './App.svelte';
+
+var app = new App({
+	target: document.body
+});
+
+export default app;
