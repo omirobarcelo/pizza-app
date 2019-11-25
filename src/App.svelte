6137bd847e286d5f4e26abdbe3c86fbfa054ea51
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2019-11-25 10:34:43 +0100

    Moved navigation to header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,58 +1,14 @@
 <script>
-  import { onDestroy } from "svelte";
-  import Contact from "./pages/Contact.svelte";
-  import PageNotFound from "./pages/PageNotFound.svelte";
-  import PizzaCreator from "./pages/PizzaCreator.svelte";
+  import Header from "./components/Header.svelte";
 
-  const routes = [
-    { route: "", page: PizzaCreator },
-    { route: "#creator", page: PizzaCreator },
-    { route: "#contact", page: Contact }
-  ];
-  let selectedRoute = routes[0];
-
-  const pageSelector = () =>
-    (selectedRoute = routes.find(
-      route => route.route === window.location.hash
-    ));
-  window.addEventListener("hashchange", pageSelector);
-
-  onDestroy(() => window.removeEventListener("hashchange", pageSelector));
+  let activePage;
 </script>
 
 <div class="main-container">
-  <header class="header header-6">
-    <div class="branding">
-      <a href="./#creator" class="nav-link">
-        <img
-          src="favicon.png"
-          alt="Pizza App icon"
-          width="36"
-          height="36"
-          style="margin-left: 4px;" />
-        <span class="title">Shoguneko's Pizza</span>
-      </a>
-    </div>
-    <div class="header-nav">
-      <a
-        href="./#creator"
-        class="nav-link nav-text"
-        class:active={selectedRoute && (selectedRoute.route === '' || selectedRoute.route === '#creator')}>
-        Pizza Creator
-      </a>
-      <a
-        href="./#contact"
-        class="nav-link nav-text"
-        class:active={selectedRoute && selectedRoute.route === '#contact'}>
-        Contact
-      </a>
-    </div>
-  </header>
+  <Header on:activePage={evt => (activePage = evt.detail.page)} />
   <div class="content-container">
-    {#if selectedRoute}
-      <svelte:component this={selectedRoute.page} />
-    {:else}
-      <PageNotFound />
+    {#if activePage}
+      <svelte:component this={activePage} />
     {/if}
   </div>
 </div>
