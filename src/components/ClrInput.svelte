7920b2dc9ac49eabbb01d51c3fb4a066cf1f8563
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2019-12-03 12:28:37 +0100

    Created form elements components

---
+++ src/components/ClrInput.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let type;
+  export let name;
+  export let placeholder = undefined;
+  export let label = undefined;
+  export let helperText = undefined;
+
+  let input;
+  let invalid = true;
+  onMount(() => (invalid = !input.validity.valid));
+</script>
+
+<div class="clr-form-control">
+  {#if label}
+    <label for={name} class="clr-control-label">{label}</label>
+  {/if}
+  <div class="clr-control-container" class:clr-error={invalid}>
+    <div class="clr-input-wrapper">
+      <input
+        bind:this={input}
+        {type}
+        {name}
+        {placeholder}
+        class="clr-input"
+        on:input={() => (invalid = !input.validity.valid)} />
+      <clr-icon class="clr-validate-icon" shape="exclamation-circle" />
+    </div>
+    {#if helperText}
+      <span class="clr-subtext">{helperText}</span>
+    {/if}
+  </div>
+</div>
---
+++ src/components/ClrTextarea.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let name;
+  export let placeholder = undefined;
+  export let label = undefined;
+  export let helperText = undefined;
+
+  let textarea;
+  let invalid = true;
+  onMount(() => (invalid = !textarea.validity.valid));
+</script>
+
+<div class="clr-form-control">
+  {#if label}
+    <label for={name} class="clr-control-label">{label}</label>
+  {/if}
+  <div class="clr-control-container" class:clr-error={invalid}>
+    <div class="clr-textarea-wrapper">
+      <textarea
+        bind:this={textarea}
+        {name}
+        {placeholder}
+        class="clr-textarea"
+        on:input={() => (invalid = !textarea.validity.valid)} />
+      <clr-icon class="clr-validate-icon" shape="exclamation-circle" />
+    </div>
+    {#if helperText}
+      <span class="clr-subtext">{helperText}</span>
+    {/if}
+  </div>
+</div>
