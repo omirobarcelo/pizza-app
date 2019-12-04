d6f6b2c21ced3ba1a61e22b764d54df8432cc11a
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2019-12-04 13:08:17 +0100

    Creator page layout

--- src/pages/PizzaCreator.svelte
+++ src/pages/PizzaCreator.svelte
@@ -1,1 +1,40 @@
-<div>Pizza Creator</div>
+<style type="text/scss">
+  $header-height: 96px;
+
+  .header-area {
+    width: 100%;
+    height: $header-height;
+  }
+
+  .creator-container {
+    display: flex;
+    height: calc(100% - #{$header-height});
+  }
+
+  .sidebar {
+    flex: 0 276px;
+  }
+
+  .main-area {
+    flex: 1;
+  }
+
+  @media (max-width: 576px) {
+    .creator-container {
+      flex-direction: column;
+    }
+
+    .sidebar {
+      flex: 1;
+      order: 1;
+    }
+  }
+</style>
+
+<div class="content-area" style="padding: 0;">
+  <div class="header-area" style="background-color: blue;" />
+  <div class="creator-container">
+    <div class="sidebar" style="background-color: red;" />
+    <div class="main-area" style="background-color: yellow;" />
+  </div>
+</div>
