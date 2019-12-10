0d6d7087f5e5320b3f20adcca69dbb839b635243
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2019-12-10 12:17:13 +0100

    Created Pizza Image component, added deatils and image to creator

---
+++ src/components/PizzaImage.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { pizza } from "../stores/pizza.js";
+</script>
+
+<style type="text/scss">
+  img {
+    margin: 10px 0;
+    max-width: 100%;
+  }
+
+  .extra {
+    position: absolute;
+    left: 0;
+  }
+</style>
+
+<div style="position: relative;">
+  {#if $pizza.type}
+    <img
+      alt={$pizza.name}
+      src={`https://raw.githubusercontent.com/omirobarcelo/pizza-app/master/assets/pizzas/${$pizza.type.id}.png`} />
+  {/if}
+  {#each $pizza.extras as extra}
+    <img
+      class="extra"
+      alt={extra.name}
+      src={`https://raw.githubusercontent.com/omirobarcelo/pizza-app/master/assets/extras/${extra.id}_${extra.quantity > 3 ? 3 : extra.quantity}.png`} />
+  {/each}
+</div>
--- src/pages/PizzaCreator.svelte
+++ src/pages/PizzaCreator.svelte
@@ -3,6 +3,8 @@
   import SidebarPizza from "../components/SidebarPizza.svelte";
   import SidebarExtras from "../components/SidebarExtras.svelte";
   import PizzaDetails from "../components/PizzaDetails.svelte";
+  import PizzaImage from "../components/PizzaImage.svelte";
+  import DeliveryForm from "../components/DeliveryForm.svelte";
   import { step } from "../stores/step.js";
 
   const sidebarComponents = {
@@ -27,11 +29,17 @@
 
   .sidebar {
     flex: 0 276px;
+    max-width: 276px;
   }
 
   .main-area {
     flex: 1;
     border-top: 1px solid #e0e0e0;
+    overflow: auto;
+  }
+
+  .details-container {
+    max-height: calc(100vh - #{$header-height});
   }
 
   @media (max-width: 736px) {
@@ -42,6 +50,15 @@
     .sidebar {
       flex: 1;
       order: 1;
+      max-width: unset;
+    }
+
+    .main-area {
+      overflow: unset;
+    }
+
+    .details-container {
+      max-height: none;
     }
   }
 </style>
@@ -55,7 +72,22 @@
       <svelte:component this={sidebarComponents[$step]} sidebar={true} />
     </div>
     <div class="main-area">
-      <PizzaDetails />
+      {#if $step === 3}
+        <div class="clr-row" style="margin: 0;">
+          <div class="clr-offset-md-2 clr-col-md-7 clr-offset-1 clr-col-10">
+            <DeliveryForm />
+          </div>
+        </div>
+      {:else}
+        <div class="clr-row" style="margin: 0;">
+          <div class="clr-offset-md-3 clr-col-md-6 clr-offset-1 clr-col-10">
+            <div class="details-container">
+              <PizzaImage />
+              <PizzaDetails />
+            </div>
+          </div>
+        </div>
+      {/if}
     </div>
   </div>
 </div>
